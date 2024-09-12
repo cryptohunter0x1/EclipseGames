43f1d27030dfa41a3e21a024f71e3f7aebf57bb7
root <root@vmi2111326.contaboserver.net>
2024-09-12 16:22:36 +0200

    Ajout des fichiers du projet Tic Tac Toe

---
+++ tic_tac_toe_contract/programs/tic_tac_toe_contract/src/lib.rs
@@ -0,0 +1,17 @@
+#![allow(unstable_features)]
+#![feature(build_hasher_simple_hash_one)]
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod tic_tac_toe_contract {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
